88add29104eaf5f97d66c6d726d01be51fe4d774
Anurag Nagar <anuragintensenagar1999@gmail.com>
2023-06-19 17:29:49 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+__starter__ = """
+x---------------------------------x
+|     WELCOME TO OUR QUIZ GAME    |
+x---------------------------------x
+"""
+
+print(__starter__)
+
+player = input("Enter your name:\n>>")
